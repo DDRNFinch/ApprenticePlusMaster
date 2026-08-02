bb857239710128cb503b3fffbcd2836711b3c88c
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-02 22:36:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -245,7 +245,7 @@ function learnerPromptTitle(assignmentNumber,code,fallback){
  return LEARNER_PROMPTS[COURSE.id]?.[assignmentNumber]?.[code]||fallback;
 }
 
-const APP_VERSION='V1.6.5';
+const APP_VERSION='V1.6.6';
 
 const TECHNICAL_DRAWING_BASE='https://ddrnfinch.github.io/ApprenticePlusMaster/drawings/';
 const TECHNICAL_DRAWING_PREFIX={
