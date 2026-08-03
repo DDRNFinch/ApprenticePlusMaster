bdee452f4a1a282c2fdc73aa5c981e390e352c3f
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-03 22:52:44 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='apprentice-plus-v1.0-reviewmate-live-targets';
+const CACHE='apprentice-plus-v1.0-reviewmate-weekly-targets-profile';
 const CURRENT_VERSION='V1.0';
 const RELEASE_NOTES_URL='./release-notes.json';
 const FALLBACK_UPDATE_INFO={
